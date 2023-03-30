5a776f88a77e423b78a1379a4d900c27f5a7d8f8
Alejandro Tauber <57751731+hackejandro@users.noreply.github.com>
2023-03-30 11:10:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-// get a reference to the tiny image
+// Get references to the HTML elements
 const tinyImage = document.querySelector('.tiny-image');
-
-// get a reference to the other images
 const bookImage = document.querySelector('.book-image');
 const geeseImage = document.querySelector('.geese-image');
 const jellyfishImage = document.querySelector('.jellyfish-image');
+const burntMarshmallow = document.querySelector('.burnt-marshmallow');
 const textMessageImage = document.querySelector('.text-message-image');
 
-// add a click event listener to the tiny image
+// Add a click event listener to the tiny image
 tinyImage.addEventListener('click', function() {
-  // show the book image and caption
-  bookImage.classList.remove('hidden');
-
-  // show the geese image
-  geeseImage.classList.remove('hidden');
-
-  // show the jellyfish images
-  jellyfishImage.classList.remove('hidden');
-
-  // show the text message image
-  textMessageImage.classList.remove('hidden');
+  // Toggle the "hidden" class on each element
+  bookImage.classList.toggle('hidden');
+  geeseImage.classList.toggle('hidden');
+  jellyfishImage.classList.toggle('hidden');
+  textMessageImage.classList.toggle('hidden');
 });
+
+// Add a function to animate the jellyfish
+function animateJellyfish() {
+  const jellyfishImages = document.querySelectorAll('.jiggle');
+  jellyfishImages.forEach(function(jellyfishImage) {
+    // Generate a random angle for the rotation
+    const rotationAngle = Math.floor(Math.random() * 30) - 15;
+    jellyfishImage.style.transform = `rotate(${rotationAngle}deg)`;
+  });
+  // Call this function again in 500 milliseconds
+  setTimeout(animateJellyfish, 500);
+}
+
+// Call the animateJellyfish function to start the animation
+animateJellyfish();
