a99dd879d004c8c7822ed96794e4b5426db5cc1b
Alejandro Tauber <57751731+hackejandro@users.noreply.github.com>
2023-03-30 11:29:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-// Get references to the HTML elements
-const tinyImage = document.querySelector('.tiny-image');
-const bookImage = document.querySelector('.book-image');
-const geeseImage = document.querySelector('.geese-image');
-const jellyfishImage = document.querySelector('.jellyfish-image');
-const burntMarshmallow = document.querySelector('.burnt-marshmallow');
-const textMessageImage = document.querySelector('.text-message-image');
+function showImages() {
+  const bookImage = document.querySelector('.book-image');
+  const geeseImage = document.querySelector('.geese-image');
+  const jellyfishImage = document.querySelector('.jellyfish-image');
+  const textMessageImage = document.querySelector('.text-message-image');
 
-// Add a click event listener to the tiny image
-tinyImage.addEventListener('click', function() {
-  // Toggle the "hidden" class on each element
-  bookImage.classList.toggle('hidden');
-  geeseImage.classList.toggle('hidden');
-  jellyfishImage.classList.toggle('hidden');
-  textMessageImage.classList.toggle('hidden');
-});
+  bookImage.classList.remove('hidden');
+  geeseImage.classList.remove('hidden');
+  jellyfishImage.classList.remove('hidden');
+  textMessageImage.classList.remove('hidden');
+}
+
+const tinyImage = document.querySelector('.tiny-image');
+tinyImage.addEventListener('click', showImages);
