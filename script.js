1587127cc8277f530d42c4a81bf2e0c9e2a8e86b
Alejandro Tauber <57751731+hackejandro@users.noreply.github.com>
2023-03-31 19:41:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ function showImages() {
   const geeseImage = document.querySelector('.geese-image');
   const jellyfishImage = document.querySelector('.jellyfish-image');
   const textMessageImage = document.querySelector('.text-message-image');
+  const tinyImage = document.querySelector('.tiny-image');
 
   bookImage.classList.remove('hidden');
   geeseImage.classList.remove('hidden');
   jellyfishImage.classList.remove('hidden');
   textMessageImage.classList.remove('hidden');
+  tinyImage.classList.add('hidden');
 }
 
 const tinyImage = document.querySelector('.tiny-image');
