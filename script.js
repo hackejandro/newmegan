5e6826744e392f6030e1319c5770eed9fa02c948
Alejandro Tauber <57751731+hackejandro@users.noreply.github.com>
2023-03-31 20:53:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,17 +17,17 @@ const tinyImage = document.querySelector('.tiny-image');
 tinyImage.addEventListener('click', showImages);
 console.log('Click event triggered');
 
-const bookImage = document.querySelector('.book-image img');
-const kopenText = document.createElement('div');
-kopenText.classList.add('kopen-text');
-kopenText.innerText = 'KOPEN';
-document.body.appendChild(kopenText);
+// const bookImage = document.querySelector('.book-image img');
+// const kopenText = document.createElement('div');
+// kopenText.classList.add('kopen-text');
+// kopenText.innerText = 'KOPEN';
+// document.body.appendChild(kopenText);
 
-bookImage.addEventListener('mouseover', () => {
-  kopenText.classList.add('move-down');
-});
+// bookImage.addEventListener('mouseover', () => {
+//   kopenText.classList.add('move-down');
+// });
 
-bookImage.addEventListener('mouseleave', () => {
-  kopenText.classList.remove('move-down');
-});
+// bookImage.addEventListener('mouseleave', () => {
+//   kopenText.classList.remove('move-down');
+// });
 
